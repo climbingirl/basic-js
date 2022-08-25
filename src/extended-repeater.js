9582c500122fd0628421778e050e03b5436aa20a
climbingirl <isvetlanaivanova@yandex.ru>
2022-08-25 18:21:20 +0500

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const separator = options.separator || '+';
+  const additionSeparator = options.additionSeparator || '|';
+  const addition = ('addition' in options) ? options.addition + '' : '';
+
+  const makeStr = (str, separator, times) => {
+    let res = str;
+
+    for (let i = 1; i < times; i++) {
+      res += separator + str;
+    }
+
+    return res;
+  }
+
+  const additionStr = makeStr(addition, additionSeparator, options.additionRepeatTimes);
+  const string = str + additionStr;
+  let result = makeStr(string, separator, options.repeatTimes);
+
+  return result;
 }
 
 module.exports = {
