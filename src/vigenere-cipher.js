172b6a71c2473efe61536b207315115b4ded3902
climbingirl <isvetlanaivanova@yandex.ru>
2022-09-02 20:12:32 +0500

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const fullKey = key.padEnd(message.length, key);
+    let encryptedMessage = '';
+
+    for (let i = 0, j = 0; i < message.length; i++, j++) {
+      const messageEl = message[i];
+      const keyEl = fullKey[j];
+
+       if (!alphabet.includes(messageEl)) {
+        encryptedMessage += messageEl;
+        j--;
+        continue;
+       }
+
+       const id = alphabet.indexOf(messageEl) + alphabet.indexOf(keyEl);
+       encryptedMessage += (id < 26) ? alphabet[id] : alphabet[id - 26];
+    }
+    
+    const reversedEncruptedMesssage = encryptedMessage.split('').reverse().join('');
+
+    return (this.direct === true) ? encryptedMessage : reversedEncruptedMesssage;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw Error('Incorrect arguments!');
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const fullKey = key.padEnd(encryptedMessage.length, key);
+    let decryptedMessage = '';
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++, j++) {
+      const encryptedEl = encryptedMessage[i];
+      const keyEl = fullKey[j];
+
+      if (!alphabet.includes(encryptedEl)) {
+        decryptedMessage += encryptedEl;
+        j--;
+        continue;
+      }
+
+      const id = alphabet.indexOf(encryptedEl) - alphabet.indexOf(keyEl);
+      decryptedMessage += (id >= 0) ? alphabet[id] : alphabet[id + 26];
+    }
+
+    const reversedDecruptedMesssage = decryptedMessage.split('').reverse().join('');
+
+    return (this.direct === true) ? decryptedMessage : reversedDecruptedMesssage;
   }
 }
 
